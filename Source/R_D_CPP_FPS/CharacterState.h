7e47ecc16f85ce8074bfcea7f30df44969bea7c3
STRAIKERE <straikere2403@gmail.com>
2024-02-28 14:24:21 +0200

    Add CharacterState

---
+++ Source/R_D_CPP_FPS/CharacterState.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "CharacterState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class R_D_CPP_FPS_API ACharacterState : public APlayerState
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
