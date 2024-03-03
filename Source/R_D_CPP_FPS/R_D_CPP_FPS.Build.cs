7cda198f949be0bb39e1069cc3ed6b849f04cef3
STRAIKERE <straikere2403@gmail.com>
2024-03-03 09:49:26 +0200

    Add Message Subsystem

--- Source/R_D_CPP_FPS/R_D_CPP_FPS.Build.cs
+++ Source/R_D_CPP_FPS/R_D_CPP_FPS.Build.cs
@@ -6,9 +6,22 @@ public class R_D_CPP_FPS : ModuleRules
 {
 	public R_D_CPP_FPS(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput"
+		});
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"GameplayAbilities", 
+			"GameplayTags", 
+			"GameplayTasks",
+			"GameplayMessageRuntime",
+		});
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 	}
 }
